d8dcc0cde075bd801a6af29329b4c2fb5048c149
Hotpause <ashutoshagarwal1006@gmail.com>
2025-01-10 13:27:42 +0530

    Initial commit

---
+++ Models/Client.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Logging.Abstractions;
+
+namespace MyApp.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+
+        public List<ItemClient>? ItemClients { get; set; }
+    }
+}
